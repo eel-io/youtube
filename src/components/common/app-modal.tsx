d588462e473ebf6c430699e4afbe05ee8649e3a7
wlianfu <wlianfu@163.com>
2019-07-15 14:30:32 +0800

    update tag

---
+++ src/components/common/app-modal.tsx
@@ -0,0 +1,24 @@
+// import React, { useEffect, ReactNode } from 'react';
+// import { createPortal } from 'react-dom';
+// import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+// interface IProps extends RouteComponentProps {
+//   children: ReactNode,
+// };
+// interface IState {};
+
+// const AppModal = (props: IProps, state: IState) => {
+
+//   useEffect(() => {
+//     console.log('modal props::: ', props);
+//   }, []);
+
+//   return (
+//     createPortal(
+//       props.children,
+//       document.getElementById('modal') as HTMLElement,
+//     )
+//   )
+// }
+
+// export default withRouter(AppModal);
--- src/components/popular/index.tsx
+++ src/components/popular/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
+import Button from '@material-ui/core/Button';
 
 import AppNav from '@comp/common/app-nav';
+import '@scss/popular/main';
 
 interface IProps {};
 interface IState {};
@@ -12,8 +14,11 @@ const Popular = (props: IProps, state: IState) => {
   }, []);
 
   return (
-    <div>
+    <div className="popular-page">
       <p>Popular Page</p>
+      <p>
+        <Button variant="contained" color="primary">Portal</Button>
+      </p>
 
       <AppNav value="popular" />
     </div>
