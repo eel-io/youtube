ddac08e1fe6875021fb4498ddc735e6c01fce17c
wangzhenfu <wangzhenfu@zto.com>
2020-03-18 01:10:42 +0800

    添加备案号

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -24,6 +24,9 @@ const Home = (props: IProps, state: IState) => {
         <List />
         <List />
         <p>End ---</p>
+        <footer>
+          <a href="http://beian.miit.gov.cn/" target="_blank">浙ICP备17060547号</a>
+        </footer>
       </section>
 
       <AppNav value="home" />
